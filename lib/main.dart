c3178240d3caec264ac36f870227c5487049d070
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-29 10:58:31 -0300

    feat: add toString to tarefa

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,11 @@ class Tarefa {
   final String descricao;
   final String obs;
   Tarefa(this.descricao, this.obs); // construtor
+
+  @override
+  String toString() {
+    return 'Tarefa{descricao: $descricao, obs: $obs}';
+  }
 }
 
 // card de uma tarefa
