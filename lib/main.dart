0f33c3a68eb8e21a05c3f53af603b045b08ddd98
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-29 11:21:58 -0300

    feat: add list of tarefas on ListaTarefa

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ void main() {
 class TarefaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( // widget pai do app com Material
-        home: ListaTarefa()
-    );
+    return MaterialApp(
+        // widget pai do app com Material
+        home: ListaTarefa());
     throw UnimplementedError();
   }
 }
@@ -28,7 +28,8 @@ class Tarefa {
 }
 
 // card de uma tarefa
-class ItemTarefa extends StatelessWidget { // widget estático
+class ItemTarefa extends StatelessWidget {
+  // widget estático
   final Tarefa _tarefa; // convenção de nome de objetos privados com _
   const ItemTarefa(this._tarefa); // ao ser chamado, deve receber uma Tarefa
 
@@ -47,29 +48,42 @@ class ItemTarefa extends StatelessWidget { // widget estático
 
 // lista de tarefas
 class ListaTarefa extends StatelessWidget {
+  final List<Tarefa> _tarefas = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( // widget da página home
-      appBar: AppBar( // widget da barra superior
+    _tarefas.add((Tarefa("Estudar Flutter", "Ler documentação")));
+    _tarefas.add((Tarefa("Fazer trabalho", "TDM")));
+
+    return Scaffold(
+      // widget da página home
+      appBar: AppBar(
+        // widget da barra superior
         title: Text("Lista de Tarefas"), // título da página
       ),
-      body: Column( // lista das tarefas em uma coluna de cards
-        children: [
-          ItemTarefa((Tarefa("Estudar Flutter", "Ler documentação"))),
-          ItemTarefa((Tarefa("Ler livro", "Percy Jackson"))),
-          ItemTarefa((Tarefa("Fazer trabalho", "TDM"))),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton( // botão flutuante
-        onPressed: (){
+      body: ListView.builder(
+          // componente automático de listagem
+          itemCount:
+              _tarefas.length, // tamanho da lista é a quantidade de items
+          itemBuilder: (context, indice) {
+            // retorna um ItemTarefa para cada tarefa da lista
+            final tarefa = _tarefas[indice];
+            return ItemTarefa(tarefa);
+          }),
+      floatingActionButton: FloatingActionButton(
+        // botão flutuante
+        onPressed: () {
           print("Pressionou botão"); // print no terminal ao pressionar
           // future indica o retorno futuro (ex.: alguma info da tarefa que será feita)
-          final Future future = Navigator.push(context, // push indica adicionar na pilha Navigator
-              MaterialPageRoute(builder: (context){ // gerenciador de rotas
-                return FormTarefa(); // abre FormTarefa na nova tela
-              }));
-          future.then((tarefa){
+          final Future future = Navigator.push(
+              context, // push indica adicionar na pilha Navigator
+              MaterialPageRoute(builder: (context) {
+            // gerenciador de rotas
+            return FormTarefa(); // abre FormTarefa na nova tela
+          }));
+          future.then((tarefa) {
             print('Tarefa retornada no future: $tarefa');
+            _tarefas.add(tarefa); // adicionar tarefa na lista
           });
         },
         child: Icon(Icons.add),
@@ -88,30 +102,40 @@ class FormTarefa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Adicionar Tarefa"),),
+      appBar: AppBar(
+        title: Text("Adicionar Tarefa"),
+      ),
       body: Column(
         children: [
           // adicionar espaçamento -> padding
-          Padding(padding: const EdgeInsets.all(16.0), // padding de 16pt em todos os lados
-              child: TextField(style: TextStyle(fontSize: 16.0),
+          Padding(
+            padding:
+                const EdgeInsets.all(16.0), // padding de 16pt em todos os lados
+            child: TextField(
+              style: TextStyle(fontSize: 16.0),
               controller: _controladorTarefa,
-                decoration: InputDecoration(
-                icon: Icon(Icons.add_alert),
-                labelText: "Tarefa",
-                hintText: "Indique a tarefa"),),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.add_alert),
+                  labelText: "Tarefa",
+                  hintText: "Indique a tarefa"),
+            ),
           ),
-          Padding(padding: const EdgeInsets.all(16.0), // padding de 16pt em todos os lados
-            child: TextField(style: TextStyle(fontSize: 16.0),
+          Padding(
+            padding:
+                const EdgeInsets.all(16.0), // padding de 16pt em todos os lados
+            child: TextField(
+              style: TextStyle(fontSize: 16.0),
               controller: _controladorObs,
               decoration: InputDecoration(
                   icon: Icon(Icons.emoji_objects),
                   labelText: "Observação",
-                  hintText: "Indique a observação da tarefa"),),
+                  hintText: "Indique a observação da tarefa"),
+            ),
           )
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           criarTarefa(context);
         },
         child: Icon(Icons.save),
