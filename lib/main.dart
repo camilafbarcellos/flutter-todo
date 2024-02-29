3a7b118153afc151375230a878324b1842f1053b
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-29 10:31:35 -0300

    feat: add tarefa app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp( // widget pai do app com Material
-    home: Scaffold( // widget da página home
-      appBar: AppBar( // widget da barra superior
-        title: Text("Lista de Tarefas"), // título da página
-      ),
-      body: ListaTarefa(),
-      floatingActionButton: FloatingActionButton( // botão flutuante
-        onPressed: (){
-          print("Pressionou botão"); // print no terminal ao pressionar
-        },
-        child: Icon(Icons.add),
-      ),
-    )
-  ));
+  runApp(TarefaApp());
+}
+
+// material app da lista de tarefas
+class TarefaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( // widget pai do app com Material
+        home: Scaffold( // widget da página home
+          appBar: AppBar( // widget da barra superior
+            title: Text("Lista de Tarefas"), // título da página
+          ),
+          body: ListaTarefa(),
+          floatingActionButton: FloatingActionButton( // botão flutuante
+            onPressed: (){
+              print("Pressionou botão"); // print no terminal ao pressionar
+            },
+            child: Icon(Icons.add),
+          ),
+        )
+    );
+    throw UnimplementedError();
+  }
+
 }
 
 // objeto da tarefa
