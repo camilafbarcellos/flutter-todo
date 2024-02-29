2a54d0e6e9bafe5e7d8f8ed845b8b5687cf90296
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-29 10:13:49 -0300

    feat: add appbar and cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp( // widget pai do app com Material
+    home: Scaffold( // widget da página home
+      appBar: AppBar( // widget da barra superior
+        title: Text("Lista de Tarefas"), // título da página
+      ),
+      body: Column( // lista das tarefas em uma coluna de cards
+        children: [
+          Card(
+            child: ListTile( // tarefa
+              leading: Icon(Icons.add_alert),
+              title: Text("Organizar aula TDM"),
+              subtitle: Text("Publicar no classroom"),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.add_alert),
+              title: Text("Estudar Flutter"),
+              subtitle: Text("Ler documentação e codar"),
+            ),
+          ),
+        ],
+      ),
+    )
+  ));
 }
 
