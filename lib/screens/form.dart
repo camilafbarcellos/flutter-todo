298750a6cb398d9e3a36d1b998666b2e47d2d99c
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-03-07 11:26:33 -0300

    feat: add save and update tarefa to db

--- lib/screens/form.dart
+++ lib/screens/form.dart
@@ -5,6 +5,9 @@ import '/model/tarefa.dart';
 
 // tela do formulário
 class FormTarefa extends StatefulWidget {
+  // ao editar uma tarefa, o objeto vem preenchido
+  final Tarefa? tarefa; // pode chegar com valor nulo (inclusão de tarefa)
+  FormTarefa({this.tarefa}); // {} -> opcionalidade
   // controladores para capturar os dados da tarefa
   final TextEditingController _controladorTarefa = TextEditingController();
   final TextEditingController _controladorObs = TextEditingController();
@@ -16,6 +19,20 @@ class FormTarefa extends StatefulWidget {
 }
 
 class FormTarefaState extends State<FormTarefa> {
+  int? _id; // pode conter valor nulo (inclusão de tarefa)
+
+  // preparar form para receber edição de tarefa
+  @override
+  void initState() {
+    super.initState();
+    // caso seja alteração
+    if (widget.tarefa != null) {
+      _id = widget.tarefa!.id; // ! indica garantia de não-nulo
+      widget._controladorTarefa.text = widget.tarefa!.descricao;
+      widget._controladorObs.text = widget.tarefa!.obs;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,17 +60,27 @@ class FormTarefaState extends State<FormTarefa> {
   // método para criar tarefa
   void criarTarefa(BuildContext context) {
     TarefaDao _dao = TarefaDao();
-    // captura dados da tarefa pelos controladores
-    final tarefaCriada =
-        Tarefa(0, widget._controladorTarefa.text, widget._controladorObs.text);
-    _dao.save(tarefaCriada).then((id) {
-      print(tarefaCriada);
-      // mandar tarefa criada para a tela de lista tarefas pelo Navigator
-      Navigator.pop(context, tarefaCriada);
-      // exibir mensagem snackbar avisando que a tarefa foi criada
-      final SnackBar snackBar = SnackBar(content: const Text("Tarefa criada!"));
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      _dao.findAll().then((tarefa) => print(tarefa.toString()));
-    });
+    // checar se é alteração ou inclusão
+    if (_id != null) {
+      // alteração
+      // captura dados da tarefa pelos controladores
+      final tarefaCriada = Tarefa(
+          _id!, widget._controladorTarefa.text, widget._controladorObs.text);
+      _dao.update(tarefaCriada).then((id) {
+        // mandar tarefa criada para a tela de lista tarefas pelo Navigator
+        Navigator.pop(context, tarefaCriada);
+      });
+    } else {
+      // inclusão
+      final tarefaCriada = Tarefa(
+          0, widget._controladorTarefa.text, widget._controladorObs.text);
+      _dao.save(tarefaCriada).then((id) {
+        // mandar tarefa criada para a tela de lista tarefas pelo Navigator
+        Navigator.pop(context, tarefaCriada);
+      });
+    }
+    // exibir mensagem snackbar avisando que a tarefa foi criada
+    final SnackBar snackBar = SnackBar(content: const Text("Tarefa criada!"));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
