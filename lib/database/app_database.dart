f4736bed7355ed80d36dc6af63a6cae5f4bc1f44
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-03-07 11:01:10 -0300

    refactor: refactor tarefa table creation

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,14 +1,12 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'tarefa_dao.dart';
 
 // método para recuperar ref do banco de dados no futuro
 Future<Database> getDatabase() async {
   final String path = join(await getDatabasesPath(), 'dbtarefas.db');
   return openDatabase(path, onCreate: (db, version) {
     // criação do database (primeira execução do app cria a tabela)
-    db.execute('CREATE TABLE tarefas ('
-        'id INTEGER PRIMARY KEY, '
-        'descricao TEXT, '
-        'obs TEXT)');
+    db.execute(TarefaDao.tableSQL);
   }, version: 1);
 }
