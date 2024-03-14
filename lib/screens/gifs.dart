8c769214b805f61fec3040bcd2e59b9869e29d3e
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-03-14 10:27:02 -0300

    feat: add initial gifs page

---
+++ lib/screens/gifs.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GifsPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return GifsPageState();
+  }
+}
+
+class GifsPageState extends State<GifsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Gifs'),
+    );
+  }
+}
