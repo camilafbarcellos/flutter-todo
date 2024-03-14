fdc3964d35606f0c99df926b33791fd33e0665aa
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-03-14 08:58:11 -0300

    feat: add new table creation when version changes

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -4,9 +4,23 @@ import 'tarefa_dao.dart';
 
 // método para recuperar ref do banco de dados no futuro
 Future<Database> getDatabase() async {
+  const String tableSQL2 = 'CREATE TABLE cursos ('
+      'id INTEGER PRIMARY KEY, '
+      'nome TEXT, '
+      'descricao TEXT)';
+
   final String path = join(await getDatabasesPath(), 'dbtarefas.db');
   return openDatabase(path, onCreate: (db, version) {
     // criação do database (primeira execução do app cria a tabela)
     db.execute(TarefaDao.tableSQL);
-  }, version: 1);
+  }, onUpgrade: (db, oldVersion, newVersion) async {
+    var batch = db.batch();
+    print("Versão atinga: " + oldVersion.toString());
+    print("Versão nova: " + newVersion.toString());
+    if (newVersion == 2) {
+      batch.execute(tableSQL2);
+    }
+    print("Criando nova tabela");
+    await batch.commit();
+  }, version: 2);
 }
