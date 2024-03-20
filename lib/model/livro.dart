2629aaced6bb82d9b05a074b4d88042901e21dd5
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-03-20 12:31:58 -0300

    feat: add livro model

---
+++ lib/model/livro.dart
@@ -0,0 +1,14 @@
+class Livro {
+  final int id;
+  final String nome;
+  final String autor;
+  final String editora;
+  final int ano;
+  final int lido;
+  Livro(this.id, this.nome, this.autor, this.editora, this.ano, this.lido);
+
+  @override
+  String toString() {
+    return 'Livro{nome: $nome, autor: $autor, editora: $editora, ano: $ano, lido: $lido}';
+  }
+}
