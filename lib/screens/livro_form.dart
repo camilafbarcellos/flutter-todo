c5fad99cc76f3aab3990c218ee7a08d806fe8a42
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-03-21 22:12:19 -0300

    fix: fix radio selection and livro update

--- lib/screens/livro_form.dart
+++ lib/screens/livro_form.dart
@@ -13,7 +13,7 @@ class FormLivro extends StatefulWidget {
   final TextEditingController _controladorAutor = TextEditingController();
   final TextEditingController _controladorEditora = TextEditingController();
   final TextEditingController _controladorAno = TextEditingController();
-  int? _lido; // integer se leu ou nao
+  int? _controladorLido; // integer se leu ou nao
 
   @override
   State<StatefulWidget> createState() {
@@ -36,9 +36,7 @@ class FormLivroState extends State<FormLivro> {
       widget._controladorAutor.text = widget.livro!.autor;
       widget._controladorEditora.text = widget.livro!.editora;
       widget._controladorAno.text = widget.livro!.ano;
-      widget._lido = widget.livro!.lido;
-      // widget._controladorLido.text =
-      //     widget.livro!.lido.toString(); // usar int.parse
+      widget._controladorLido = widget.livro!.lido;
     }
   }
 
@@ -50,151 +48,157 @@ class FormLivroState extends State<FormLivro> {
       ),
       body: Form(
         key: _formLivroKey,
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Preenchimento obrigatório!';
-                  }
-                  return null;
-                },
-                controller: widget._controladorLivro,
-                decoration: InputDecoration(
-                  labelText: 'Livro',
-                  hintText: 'Indique o livro',
-                  prefixIcon: Padding(
-                    padding: EdgeInsets.all(0.0),
-                    child: Icon(Icons.auto_stories),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Preenchimento obrigatório!';
+                    }
+                    return null;
+                  },
+                  controller: widget._controladorLivro,
+                  decoration: InputDecoration(
+                    labelText: 'Livro',
+                    hintText: 'Indique o livro',
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Icon(Icons.auto_stories),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Preenchimento obrigatório!';
-                  }
-                  return null;
-                },
-                controller: widget._controladorAutor,
-                decoration: InputDecoration(
-                  labelText: 'Autor',
-                  hintText: 'Indique o autor',
-                  prefixIcon: Padding(
-                    padding: EdgeInsets.all(0.0),
-                    child: Icon(Icons.person),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Preenchimento obrigatório!';
+                    }
+                    return null;
+                  },
+                  controller: widget._controladorAutor,
+                  decoration: InputDecoration(
+                    labelText: 'Autor',
+                    hintText: 'Indique o autor',
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Icon(Icons.person),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Preenchimento obrigatório!';
-                  }
-                  return null;
-                },
-                controller: widget._controladorEditora,
-                decoration: InputDecoration(
-                  labelText: 'Editora',
-                  hintText: 'Indique a editora',
-                  prefixIcon: Padding(
-                    padding: EdgeInsets.all(0.0),
-                    child: Icon(Icons.account_balance),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Preenchimento obrigatório!';
+                    }
+                    return null;
+                  },
+                  controller: widget._controladorEditora,
+                  decoration: InputDecoration(
+                    labelText: 'Editora',
+                    hintText: 'Indique a editora',
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Icon(Icons.account_balance),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextFormField(
-                validator: (value) {
-                  final regex = new RegExp(r'^[0-9]+$');
-                  if (value == null || value.isEmpty) {
-                    return 'Preenchimento obrigatório!';
-                  } else if (!regex.hasMatch(value)) {
-                    return 'O ano deve ser um número inteiro!';
-                  }
-                  return null;
-                },
-                controller: widget._controladorAno,
-                decoration: InputDecoration(
-                  labelText: 'Ano',
-                  hintText: 'Indique o ano de publicaçao',
-                  prefixIcon: Padding(
-                    padding: EdgeInsets.all(0.0),
-                    child: Icon(Icons.event),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: TextFormField(
+                  validator: (value) {
+                    final regex = new RegExp(r'^[0-9]+$');
+                    if (value == null || value.isEmpty) {
+                      return 'Preenchimento obrigatório!';
+                    } else if (!regex.hasMatch(value)) {
+                      return 'O ano deve ser um número inteiro!';
+                    }
+                    return null;
+                  },
+                  controller: widget._controladorAno,
+                  decoration: InputDecoration(
+                    labelText: 'Ano',
+                    hintText: 'Indique o ano de publicação',
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Icon(Icons.event),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Text('Já leu?'),
-                  SizedBox(width: 10),
-                  Row(
-                    children: [
-                      Radio<int>(
-                        value: 1,
-                        groupValue: widget._lido,
-                        onChanged: (value) {
-                          setState(() {
-                            widget._lido = value as int;
-                          });
-                        },
-                      ),
-                      Text('Sim'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Radio<int>(
-                        value: 0,
-                        groupValue: widget._lido,
-                        onChanged: (value) {
-                          setState(() {
-                            widget._lido = value as int;
-                          });
-                        },
-                      ),
-                      Text('Não'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              width: 125,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formLivroKey.currentState!.validate()) {
-                    criarLivro(context);
-                  }
-                },
+              Padding(
+                padding: const EdgeInsets.all(8.0),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.save),
-                    Text(' Salvar'),
+                    Text('Já leu?'),
+                    SizedBox(width: 10),
+                    Row(
+                      children: [
+                        Radio<int>(
+                          value: 1,
+                          groupValue: widget._controladorLido,
+                          onChanged: (value) {
+                            setState(() {
+                              widget._controladorLido = value;
+                            });
+                          },
+                        ),
+                        Text('Sim'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Radio<int>(
+                          value: 0,
+                          groupValue: widget._controladorLido,
+                          onChanged: (value) {
+                            setState(() {
+                              widget._controladorLido = value;
+                            });
+                          },
+                        ),
+                        Text('Não'),
+                      ],
+                    ),
                   ],
                 ),
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all<EdgeInsets>(
-                      EdgeInsets.symmetric()),
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                width: 125,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formLivroKey.currentState!.validate()) {
+                      // checar se é alteração ou inclusão
+                      if (_id != null)
+                        atualizarLivro(context);
+                      else
+                        criarLivro(context);
+                    }
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.save),
+                      Text(' Salvar'),
+                    ],
+                  ),
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.symmetric()),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -204,41 +208,39 @@ class FormLivroState extends State<FormLivro> {
   // método para criar livro
   void criarLivro(BuildContext context) {
     LivroDao _dao = LivroDao();
-    // checar se é alteração ou inclusão
-    if (_id != null) {
-      // alteração
-      // captura dados da livro pelos controladores
-      widget._controladorLivro.text = widget.livro!.nome;
-      widget._controladorAutor.text = widget.livro!.autor;
-      widget._controladorEditora.text = widget.livro!.editora;
-      widget._controladorAno.text = widget.livro!.ano;
-      widget._lido = widget.livro!.lido;
+    // inclusão
+    final livroNovo = Livro(
+        0,
+        widget._controladorLivro.text,
+        widget._controladorAutor.text,
+        widget._controladorEditora.text,
+        widget._controladorAno.text,
+        widget._controladorLido!);
 
-      final livroNovo = Livro(
-          _id!,
-          widget._controladorLivro.text,
-          widget._controladorAutor.text,
-          widget._controladorEditora.text,
-          widget._controladorAno.text,
-          widget._lido!);
-      // mandar livro criada para a tela de lista livros pelo Navigator
-      _dao.update(livroNovo).then((id) => Navigator.pop(context, livroNovo));
-    } else {
-      // inclusão
-      final livroNovo = Livro(
-          0,
-          widget._controladorLivro.text,
-          widget._controladorLivro.text,
-          widget._controladorAutor.text,
-          widget._controladorEditora.text,
-          widget._controladorAno.text as int,
-          widget._lido!);
-      // mandar livro criada para a tela de lista livros pelo Navigator
-      _dao.save(livroNovo).then((id) => Navigator.pop(context, livroNovo));
-    }
-    // exibir mensagem snackbar avisando que a livro foi criada
+    // mandar livro criado para a tela de lista livros pelo Navigator
+    _dao.save(livroNovo).then((id) => Navigator.pop(context, livroNovo));
+
+    // exibir mensagem snackbar avisando que o livro foi criado
     final SnackBar snackBar =
         SnackBar(content: const Text("Livro adicionado!"));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  // método para atualizar livro
+  void atualizarLivro(BuildContext context) {
+    LivroDao _dao = LivroDao();
+    final livroNovo = Livro(
+        _id!,
+        widget._controladorLivro.text,
+        widget._controladorAutor.text,
+        widget._controladorEditora.text,
+        widget._controladorAno.text,
+        widget._controladorLido!);
+    // mandar livro atualizado para a tela de lista livros pelo Navigator
+    _dao.update(livroNovo).then((id) => Navigator.pop(context, livroNovo));
+    // exibir mensagem snackbar avisando que o livro foi atualizado
+    final SnackBar snackBar =
+        SnackBar(content: const Text("Livro atualizado!"));
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
